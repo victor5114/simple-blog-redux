d81682912ddd2f8e66140913db8262f05f72df8e
Victor Pongnian <victor.pongnian@gmail.com>
2016-09-14 00:20:59 +0100

    post show delete feature

--- src/components/postShow.jsx
+++ src/components/postShow.jsx
@@ -1,22 +1,67 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
 
 class PostShow extends Component {
+
+  static contextTypes = {
+    router: PropTypes.object,
+  };
 
   static propTypes = {
     params: PropTypes.object.isRequired,
     fetchPost: PropTypes.func.isRequired,
+    deletePost: PropTypes.func.isRequired,
+    post: PropTypes.object,
+  }
+
+  constructor(props) {
+    super(props);
+    this.onDeleteClick = this.onDeleteClick.bind(this);
   }
 
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
   }
 
+  onDeleteClick() {
+    this.props.deletePost(this.props.params.id)
+      .then(() => {
+        // blog post has been created navigate the username
+        // to the index. We navigate by calling this.context.router.push
+        // new path to navifate to
+        this.context.router.push('/');
+      });
+  }
+
   render() {
-    return <div>Show post {this.props.params.id} </div>;
+    const { post } = this.props;
+
+    if (!this.props.post) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <Link to="/">Back to Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick}
+        >
+          Delete Post
+        </button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+    );
   }
 }
 
-export default connect(null, { fetchPost })(PostShow);
+function mapStateToProps(state) {
+  return { post: state.posts.post };
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostShow);
